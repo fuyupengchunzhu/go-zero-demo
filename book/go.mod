9fee22824def9c2deb610703d3b9099b1806dc6d
fuyupengchunzhu <fuyupeng_cs@163.com>
2022-04-16 22:55:29 +0800

    first

---
+++ book/go.mod
@@ -0,0 +1,11 @@
+module book
+
+go 1.15
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.2.0
+	github.com/zeromicro/go-zero v1.3.2
+	github.com/zeromicro/go-zero/tools/goctl v1.3.4
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
+)
---
+++ book/service/search/api/search.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/zeromicro/go-zero/core/logx"
+	"net/http"
+
+	"book/service/search/api/internal/config"
+	"book/service/search/api/internal/handler"
+	"book/service/search/api/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/rest"
+)
+
+var configFile = flag.String("f", "etc/search-api.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+
+	ctx := svc.NewServiceContext(c)
+	server := rest.MustNewServer(c.RestConf)
+	defer server.Stop()
+
+	server.Use(func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			logx.Info("global middle")
+			next(w, r)
+		}
+	})
+
+	handler.RegisterHandlers(server, ctx)
+
+	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
+	server.Start()
+}
